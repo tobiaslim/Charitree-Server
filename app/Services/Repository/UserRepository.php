ab618ed21a544ae1abf36b7015ca6c8af3020b4e
jianan <niu.jianan7@gmail.com>
2018-10-17 22:12:15 +0800

    edited user edit email

--- app/Services/Repository/UserRepository.php
+++ app/Services/Repository/UserRepository.php
@@ -40,6 +40,7 @@ class UserRepository implements IUserRepository{
      */
 
     public function edit(array $data, User $user){
+
         $user->email=$data["email"];
         $user->last_name=$data["last_name"];
         $user->first_name=$data["first_name"];
